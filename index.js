603d4eef07d445df6d03e7102100c15c0b1bea82
michael <m.bykov@gmail.com>
2014-12-11 18:03:48 +0300

    i

--- index.js
+++ index.js
@@ -4,40 +4,74 @@ var index = require('indexof');
 //var greek = require('./greek').greek;
 //var cslav = require('./cslav');
 
-var ignored = [999, 0, 8, 9, 12, 13, 16, 17, 18, 20, 27, 35, 36, 37, 38, 40]; // 13 - enter // shift = 16; ctrl = 17;
+var ignored = [999, 0, 8, 9, 12, 13, 16, 17, 18, 20, 27, 35, 36, 37, 38, 40]; // 13 - enter // shift = 16; ctrl = 17; // 46 ?
 
-// 46 ?
+/*
+  в сухом остатке -
+  keydown дает код полностью, если отслеживать e.shiftKey
+  но на русской раскладке э-ж дают ноль, увы
+*/
 
 module.exports = kc();
 
 function kc() {
     if (!(this instanceof kc)) return new kc();
     this.seq = [];
-    //var self = this;
-    //var layout = getLayout(lang);
-    // el.onkeypress = function(e) {
-    //     log('P', e.which)
-    // };
+    log('KC START');
+    return this;
 }
 
-function getLayout(lang) {
-    var path = './' + lang;
-    var layout = require(path);
-    return layout;
-}
-
-kc.prototype.lang = function(lang) {
-    log("LANG", this.seq);
-    var greek = require('./greek.js');
-    this.layout = greek;
+kc.prototype.lang = function(lcode) {
+    var langName = langCode[lcode] || 'sanskrit';
+    var fn = ['./', langName].join('');
+    //log("LANG", lcode, fn, (fn === './sanskrit'));
+    var layout = require('./sanskrit'); // WTF? ==== ONLY SANSKRIT FOR NOW ====
+    //var layout = require(fn);
+    this.layout = layout;
     return this;
 }
 
 kc.prototype.anchor = function(el) {
-    log("SET");
+    //log("SET anchor");
+    //if (!this._enabled) return;
     var that = this;
+    var seq = [];
     el.onkeydown = function(e) {
         if (index(ignored, e.which) > 0) return;
+        // if (e.which == 16) seq.push("shift");
+        // that.seq = uniq(seq);
+        //log('SHIFT', that.seq);
+        log('L', e.which, e.shiftKey, e.key);
+        that.type(e);
+    };
+    // el.onkeypress = function(e) {
+    //     if (index(ignored, e.which) > 0) return;
+    //     that.type(e);
+    // };
+    return this;
+}
+
+kc.prototype.type = function(e) {
+    var lett = this.layout[e.shiftKey][e.which];
+    if (!lett) return;
+    e.preventDefault();
+    log("TYPE", this.seq, lett, e.shiftKey);
+    var pos = e.target.selectionStart;
+    var value = e.target.value;
+    el.value = [value.substring(0, pos), lett, value.substring(pos)].join('');
+    el.setSelectionRange(pos+1, pos+1);
+}
+
+kc.prototype.OLD_TYPE = function(el) {
+    log("SET anchor");
+    this.seq = [];
+    var that = this;
+    el.onkeydown = function(e) {
+        if (e.which == 16) that.seq.push("shift");
+        //log('SHIFT', that.seq, e.shiftKey);
+    };
+    el.onkeydown = function(e) {
+        //if (index(ignored, e.which) > 0) return;
         var seq = [];
         var lett = letter(that.layout, seq, e);
         if (!lett) return;
@@ -47,30 +81,37 @@ kc.prototype.anchor = function(el) {
         el.value = [value.substring(0, pos), lett, value.substring(pos)].join('');
         el.setSelectionRange(pos+1, pos+1);
     };
-    this.seq = [];
-    return this;
 }
 
 
 function letter(layout, seq, e) {
-    var charkey = uniq(seq).concat(String.fromCharCode(e.which)).join('-').replace(/^-/,'');
+    var charkey = seq.concat(String.fromCharCode(e.which)).join('-'); //.replace(/^-/,'');
     if (!layout[charkey]) return false;
     var charcode = ['0x', layout[charkey]].join('');
     var letter = String.fromCharCode(parseInt(charcode, 16));
     if (e.which == 32) letter = ' ';
-    log('L', e.which, charkey, charcode, letter);
+    log('L', e.which, charkey, charcode, letter, e.shiftKey);
     return letter;
 }
 
-kc.prototype.type = function(e) {
-    log("TYPE", this.seq);
-    this.seq = [];
+function getLayout(lang) {
+    var path = './' + lang;
+    var layout = require(path);
+    return layout;
+}
+
+var langCode = {
+    'gr': 'greek',
+    'sa': 'sanskrit',
+    'la': 'latin',
+    'cs': 'cslav',
+    '': '',
 }
 
 
 kc.prototype.enable = function() {
     this._enabled = true;
-    //log("KC", this._enabled);
+    log("KC enabled", this._enabled);
 }
 
 kc.prototype.disable = function() {
