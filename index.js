2942c3dfb26be875f5c545ea98388f5aecfcd325
Michael Bykov <m.bykov@gmail.com>
2016-04-27 18:57:31 +0300

    minor changes

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
 
 var ignored = [999, 0, 8, 9, 12, 13, 16, 17, 18, 20, 27, 35, 36, 37, 38, 40]; // 13 - enter // shift = 16; ctrl = 17;
 
-module.exports = akshara();
+module.exports = akzara();
 
 /*
    greek is broken, .seq should be restored for combined greek syms
 */
 
-function akshara() {
-    if (!(this instanceof akshara)) return new akshara();
+function akzara() {
+    if (!(this instanceof akzara)) return new akzara();
     this.seq = [];
     return this;
 }
@@ -24,41 +24,78 @@ var langCode ={
     '': '',
 }
 
+var c = {};
+c.virama = '्';
+c.plainCons = [66, 71, 68, 90, 72, 75, 76, 77, 78, 80, 82, 87, 81, 83, 84, 86, 89, 67, 74 ];
+c.shiftCons = [ 66, 71, 68, 75, 76, 78, 80, 82, 87, 81, 83, 84, 89, 67, 74 ];
+c.a = 65;
+c.vows = [65,69,85,73,79,70,88];
 
-akshara.prototype.lang = function(lcode) {
+akzara.prototype.lang = function(lcode) {
     //var layout = require(fn);
     var layout = require('./sanskrit'); // ==== ONLY SANSKRIT FOR NOW ====
     this.layout = layout;
     return this;
 }
 
-akshara.prototype.anchor = function(el) {
+akzara.prototype.anchor = function(el) {
     var that = this;
     that.el = el;
     var seq = [];
     el.onkeydown = function(e) {
+        // log('EV', e.which)
         if (ignored.indexOf(e.which) > 0) return;
         that.type(e);
     };
     return this;
 }
 
-akshara.prototype.type = function(e) {
+// backspace = 8
+akzara.prototype.type = function(e) {
     if (!this._enabled) return;
     if(e.ctrlKey) return;
     var key = (e.shiftKey && e.altKey) ? 'altShift' : (e.shiftKey) ? 'shift' : (e.altKey) ? 'alt' : 'plain';
     var lett = this.layout[key][e.which];
     if (!lett) return;
     e.preventDefault();
+    var letvir = [lett, c.virama].join('');
+    if (key == 'plain' && inc(c.plainCons, e.which)) lett = letvir;
+    if (key == 'shift' && inc(c.shiftCons, e.which)) lett = letvir;
+    // log('lett', key, lett, letvir, inc(c.plainCons, e.which), 'WHICH', e.which, 'CONS', c.plainCons);
+    var oEd = q('#akzara');
+    var text = oEd.textContent;
+    var fin = text[text.length-1];
+    if (text.length == 0 && inc(c.vows, e.which)) lett = this.layout['alt'][e.which];
+    if (text.length > 0 && fin == c.virama && inc(c.vows, e.which)) {
+        removeVirama(e);
+        if (e.which == c.a) {
+            e.preventDefault();
+            return false;
+        }
+    }
     var lettel = document.createTextNode(lett);
-    // insertNodeAfterSelection(lettel, e);
-    log('LETT', lett)
-
+    insertNodeAfterSelection(lettel, e);
 }
 
+function removeVirama(e) {
+    var oEd = q('#akzara');
+    var text = oEd.textContent;
+    text = text.slice(0, -1);
+    oEd.textContent = text;
+    // e.preventDefault();
+    oEd.focus();
+    var range = document.createRange();
+    range.selectNodeContents(oEd);
+    range.collapse(false);
+    var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+}
 
 function insertNodeAfterSelection(node, evt) {
     var sel, range, html;
+    var element = document.getElementById('akzara');
+    // var caretOffset = 0;
     // var x = evt.clientX, y = evt.clientY;
     if (window.getSelection) {
         sel = window.getSelection();
@@ -67,13 +104,36 @@ function insertNodeAfterSelection(node, evt) {
             range.collapse(false);
             range.insertNode(node);
             range.setStartAfter(node);
+            // range.collapse(false);
+            // sel.removeAllRanges();
+            // sel.addRange(range);
+            // return;
+
+            var preCaretRange = range.cloneRange();
+            preCaretRange.selectNodeContents(element);
+            preCaretRange.setEnd(range.endContainer, range.endOffset);
+            // caretOffset = preCaretRange.toString().length;
+            // caretOffset = preCaretRange.toString();
+            var startText = preCaretRange.toString();
+            var oStart = document.createTextNode(startText);
+            log('startText', startText);
+
+            range.setEndAfter(node);
+            range.setStart(element, 0);
+            range.deleteContents();
+            range.collapse(false);
+            range.insertNode(oStart);
+            range.setStartAfter(oStart);
             sel.removeAllRanges();
             sel.addRange(range);
+            // range.setEndAfter(oStart);
+            // range.collapse(false);
         }
         // http://stackoverflow.com/questions/12920225/text-selection-in-divcontenteditable-when-double-click
         // http://stackoverflow.com/questions/6249095/how-to-set-caretcursor-position-in-contenteditable-element-div
         // http://stackoverflow.com/questions/1181700/set-cursor-position-on-contenteditable-div
-        // http://stackoverflow.com/questions/2213961/selection-ranges-in-webkit-safari-chrome <==
+        // http://stackoverflow.com/questions/2213961/selection-ranges-in-webkit-safari-chrome <=======
+        // http://stackoverflow.com/questions/20760987/how-to-get-text-from-contenteditable-div-from-beginning-to-the-cursor-position
 
     } else if (document.selection && document.selection.createRange) {
         range = document.selection.createRange();
@@ -84,11 +144,11 @@ function insertNodeAfterSelection(node, evt) {
 }
 
 
-akshara.prototype.enable = function() {
+akzara.prototype.enable = function() {
     this._enabled = true;
 }
 
-akshara.prototype.disable = function() {
+akzara.prototype.disable = function() {
     this._enabled = false;
 }
 
@@ -100,4 +160,8 @@ function q(sel) {
 
 function qs(sel) {
     return document.querySelectorAll(sel);
+}
+
+function inc(arr, item) {
+    return (arr.indexOf(item) > -1) ? true : false;
 }
